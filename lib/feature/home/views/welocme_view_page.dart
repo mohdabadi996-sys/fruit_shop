7e32aa9258caf90e8f4a1425cbf367443543d76d
mohdabadi996-sys <mohd.abadi996@gmail.com>
2026-05-17 07:49:05 +0300

    Add project source files

---
+++ lib/feature/home/views/welocme_view_page.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unnecessary_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class WelcomeViewPage extends StatelessWidget {
+  const WelcomeViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:coffee_shop/feature/home/views/welocme_view_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeViewPage(),
+    );
+  }
+}
