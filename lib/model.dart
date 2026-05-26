2a0e6d39a53a43879fcb71c5dadadb1682a13d78
Your Name <kassab.ahmad77@gmail.com>
2026-05-26 22:14:39 +0300

    26-5

--- lib/model.dart
+++ lib/model.dart
@@ -2,8 +2,25 @@
 class CategoryModel {
   /// The path to the image asset for this category.
   final String image;
+
   /// The display name of the category.
   final String name;
 
   CategoryModel({required this.image, required this.name});
+}
+
+class ProductModel {
+  final String image;
+  final String name;
+  final String price;
+  final String rate;
+  final String rateCount;
+
+  ProductModel({
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.rate,
+    required this.rateCount,
+  });
 }
